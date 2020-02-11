fe2ac7752342a92eef946ee6a8d848c14639e289
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-02-11 11:09:35 -0500

    Update debian-installer.sh

--- debian-installer.sh
+++ debian-installer.sh
@@ -8,7 +8,7 @@ case $choice in
  Y) sudo apt update && sudo apt install git libboost-all-dev build-essential python perl pkg-perl-tools libssl-dev masscan -y && sudo cpan -fi Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC;;  
 esac
 git clone https://github.com/independentcod/CCFinder.git;
-cd CCFiner/CCFinder-Linux;
+cd CCFinder/CCFinder-Linux;
 g++ ccfinder-linux.cpp -o ccfinder -lboost_filesystem -lboost_system -lboost_iostreams -lboost_regex;
 mv ./ccfinder ../../;
 ./Config;
