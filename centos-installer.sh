db322f457c275212b37ebe14e2754d3853706915
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-02-19 09:03:35 -0500

    Update centos-installer.sh

--- centos-installer.sh
+++ centos-installer.sh
@@ -5,7 +5,7 @@
   echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
   read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
   case $choice in
-   Y) sudo yum install epel-release git python perl-CPAN* boost-devel gpp openssl-devel masscan -y && sudo cpan -fi Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC;;  
+   Y) sudo yum install epel-release git python perl-CPAN* boost-devel gpp openssl-devel masscan -y && sudo cpan -fi Digest::MD5 Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC;;  
  esac
 git clone https://github.com/independentcod/CCFinder.git;
 cd CCFinder/CCFinder-Linux;
