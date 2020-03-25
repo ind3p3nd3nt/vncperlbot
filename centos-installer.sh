c89b8371b69def22cc541d1583f83a75f2f13189
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-03-24 21:45:43 -0400

    Update centos-installer.sh

--- centos-installer.sh
+++ centos-installer.sh
@@ -7,9 +7,4 @@
   case $choice in
    Y) sudo yum install epel-release git python perl-CPAN* boost-devel gpp openssl-devel masscan -y && sudo cpan -fi Digest::MD5 Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC;;  
  esac
-git clone https://github.com/independentcod/CCFinder.git;
-cd CCFinder/CCFinder-Linux;
-g++ ccfinder-linux.cpp -o ccfinder -lboost_filesystem -lboost_system -lboost_iostreams -lboost_regex;
-mv ./ccfinder ../../;
-cd ../../;
 ./Config;
