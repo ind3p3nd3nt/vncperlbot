1dc613d97692347302ca148d082c29b2f9f2ab4f
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-03-24 21:54:05 -0400

    Update debian-installer.sh

--- debian-installer.sh
+++ debian-installer.sh
@@ -6,7 +6,7 @@ echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
 read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
 case $choice in
  Y) sudo apt update;
- sudo apt install git libboost-all-dev build-essential python perl libssl-dev masscan -y;
+ sudo apt install python perl libssl-dev masscan -y;
  sudo cpan -fi Mojo::IRC IO::Socket::SSL;;  
 esac
 ./Config;
