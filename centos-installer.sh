f322adcbd4b171f6d24718feb98ddacfb3b9a2e7
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-03-27 02:44:08 -0400

    Update centos-installer.sh

--- centos-installer.sh
+++ centos-installer.sh
@@ -5,6 +5,13 @@
   echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
   read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
   case $choice in
-   Y) sudo yum install epel-release git python perl-CPAN* gpp openssl-devel masscan -y && sudo cpan -fi Digest::MD5 Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC;;  
+   Y) sudo yum install epel-release git python perl-CPAN* gpp openssl-devel masscan libfreerdp-dev libssh-dev libpostgresql-ocaml-dev libsmbclient-dev -y;
+ git clone https://github.com/vanhauser-thc/thc-hydra.git;
+ cd thc-hydra;
+ ./configure;
+ make;
+ sudo make install;
+ cd ..;
+ sudo cpan -fi Digest::MD5 Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC;  
  esac
 ./Config;
