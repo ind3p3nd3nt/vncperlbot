a857025e58e3ec46182b6fe39775490702235487
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-04 21:38:30 -0400

    Update debian-installer.sh

--- debian-installer.sh
+++ debian-installer.sh
@@ -6,13 +6,7 @@ echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
 read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
 case $choice in
  Y) sudo apt update;
- sudo apt install python perl libssl-dev masscan libfreerdp-dev libssh-dev libpostgresql-ocaml-dev libsmbclient-dev -y;
- git clone https://github.com/vanhauser-thc/thc-hydra.git;
- cd thc-hydra;
- ./configure;
- make;
- sudo make install;
- cd ..;
+ sudo apt install python perl libssl-dev masscan -y;
  sudo cpan -fi Mojo::IRC IO::Socket::SSL;;  
 esac
 ./Config;
