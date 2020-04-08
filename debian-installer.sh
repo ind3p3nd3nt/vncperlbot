ecb11305b6a7913b9475eedb0c33c353a18a58bd
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-08 02:14:51 -0400

    Update debian-installer.sh

--- debian-installer.sh
+++ debian-installer.sh
@@ -7,7 +7,8 @@ read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
 case $choice in
  Y) sudo apt update;
  sudo apt install build-essential python perl libssl-dev masscan libpcap-dev -y;
-  curl -L https://cpanmin.us | perl - --sudo App::cpanminus;
- sudo cpanm -fi Digest::MD5 Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC; 
+perl -MCPAN -e install CPAN;
+perl -MCPAN -e reload CPAN;
+sudo cpan -fi Mojo::IRC IO::Socket::SSL; 
 esac
 ./Config;
