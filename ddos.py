e8ebb694d5a7ca09c5f1c598962045734dc0229c
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-09 05:45:27 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -4,7 +4,7 @@ import random
 import socket
 import threading
 import sys
-ip, port, choice, times, threads = sys.argv[1:]
+ip, port, choice, times, threads = sys.argv[1]
 def run():
 	data = random._urandom(1024)
 	i = random.choice(("[*]","[!]","[#]"))
