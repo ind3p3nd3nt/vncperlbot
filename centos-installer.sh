2e43d644d7c78f56665e731165cb9d9211647028
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-09 04:46:33 -0400

    Update centos-installer.sh

--- centos-installer.sh
+++ centos-installer.sh
@@ -1,19 +1,19 @@
- 	#!/bin/bash
-  clear;
-  echo "Perl VNC ByPass AUTH IRC Bot with SSL";
-  echo "Made by independent";
-  echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
-  read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
-  case $choice in
-   Y) sudo yum install epel-release git python openssl-devel libpcap-devel perl-CPAN* -y;
-   sudo yum groupinstall "Development Tools" -y;
- git clone https://github.com/robertdavidgraham/masscan.git;
- cd masscan;
- make -j8;
- sudo make install;
- cd ..;
+#!/bin/bash
+clear;
+echo "Perl VNC ByPass AUTH IRC Bot with SSL";
+echo "Made by independent";
+echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
+read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
+case $choice in
+Y) sudo yum install epel-release git python openssl-devel libpcap-devel perl-CPAN* -y;
+sudo yum groupinstall "Development Tools" -y;
+git clone https://github.com/robertdavidgraham/masscan.git;
+cd masscan;
+make -j8;
+sudo make install;
+cd ..;
 perl -MCPAN -e install CPAN;
 perl -MCPAN -e reload CPAN;
 sudo cpan -fi Digest::MD5 Net::SSLeay IO::Socket::SSL Time::HiRes Mojolicious Mojo::IRC Net::Address::IP::Local; 
- esac
+esac
 ./Config;
