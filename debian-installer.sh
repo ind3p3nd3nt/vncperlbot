9fb78d44fb97b4436379bd8223b24ad847c414e0
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-09 08:02:33 -0400

    Update debian-installer.sh

--- debian-installer.sh
+++ debian-installer.sh
@@ -6,7 +6,7 @@ echo 'https://github.com/independentcod/PerlIRCSSL_VNCbypass';
 read -p "Install required packages? Y or LEAVE BLANK + ENTER" choice
 case $choice in
  Y) sudo apt-get update;
- sudo apt-get install build-essential python perl libssl-dev masscan libpcap-dev cpan-listchanges cpanminus cpanoutdated libcpan-. -y;
+ sudo apt-get install build-essential python3-dev perl libssl-dev masscan libpcap-dev cpan-listchanges cpanminus cpanoutdated libcpan-. -y;
 perl -MCPAN -e install CPAN;
 perl -MCPAN -e reload CPAN;
 sudo cpan -fi Mojo::IRC IO::Socket::SSL Net::Address::IP::Local; 
