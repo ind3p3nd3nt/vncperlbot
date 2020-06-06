ebbe8faecc47357c927b31261916f0dd3fff2ef4
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-06-06 15:11:24 -0400

    Update debian-installer.sh

--- debian-installer.sh
+++ debian-installer.sh
@@ -11,4 +11,11 @@ perl -MCPAN -e install CPAN;
 perl -MCPAN -e reload CPAN;
 sudo cpan -fi Mojo::IRC IO::Socket::SSL Net::Address::IP::Local; 
 esac
+git clone https://github.com/xymostech/loic.git;
+cd loic;
+make -j8;
+cd ..;
+sudo mv loic/loic /bin/loic;
+sudo chmod +x /bin/loic;
+rm -rf loic/
 ./Config;
